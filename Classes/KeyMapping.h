c4df2082d30dfbc6a2526bcaeabceeb9386c243d
aismann <icesoft@freenet.de>
2022-10-01 11:22:52 +0200

    Create KeyMapping.h

---
+++ Classes/KeyMapping.h
@@ -0,0 +1,41 @@
+    /**
+     * Controllers' standard  key
+     * Controller receives only standard key which contained within enum Key by default.
+     */
+    enum Key
+    {
+        KEY_NONE = 0,
+
+        JOYSTICK_LEFT_X = 1000,
+        JOYSTICK_LEFT_Y,
+        JOYSTICK_RIGHT_X,
+        JOYSTICK_RIGHT_Y,
+
+        BUTTON_A,
+        BUTTON_B,
+        BUTTON_C,
+        BUTTON_X,
+        BUTTON_Y,
+        BUTTON_Z,
+
+        BUTTON_DPAD_UP,
+        BUTTON_DPAD_DOWN,
+        BUTTON_DPAD_LEFT,
+        BUTTON_DPAD_RIGHT,
+        BUTTON_DPAD_CENTER,
+
+        BUTTON_LEFT_SHOULDER,
+        BUTTON_RIGHT_SHOULDER,
+
+        AXIS_LEFT_TRIGGER,
+        AXIS_RIGHT_TRIGGER,
+
+        BUTTON_LEFT_THUMBSTICK,
+        BUTTON_RIGHT_THUMBSTICK,
+
+        BUTTON_START,
+        BUTTON_SELECT,
+
+        BUTTON_PAUSE,
+        KEY_MAX
+    };
